6d72c6e2360700ef73f3730515598d4617448796
Labrasones <Labrasones@users.noreply.github.com>
2017-03-04 21:14:32 -0800

    Feature/platformer ai (#2)
    * Slope Climbing working
    
    going up slope should work, need to fix jumping and going down slope
    
    * Slope descending working
    
    player can descend slopes and shouldn't have problems with multiple angle changes in terrain
    
    * Decent progress on pathing
    
    * Connectivity graph complete
    
    * Update prefabs to use the new pathing script, started on pathing agent.
    
    * Remove syntax errors for merge

---
+++ Assets/Scripts/AI/PathablePlatform.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PathablePlatform : MonoBehaviour {
+
+	[Header("Pathing parameters")]
+	[Tooltip("How high can the agent jump. Do they got mad ups?")]
+	public float agentJumpHeight = 3f;
+	[Tooltip("How far forward can the agent jump from the ground")]
+	public float agentJumpDistance = 4f;
+	[Tooltip("How far the agent can move laterally for each unit it falls")]
+	public float agentFloatDistance = 0.5f;
+	[Tooltip("How far can the agent drop before it shouldn't be considered a valid move")]
+	public float agentDropHeight = 10f;
+	[Tooltip("Number of world units per pathfinding grid unit")]
+	public float gridUnit = 0.5f;
+	[Tooltip("How big is the agent traversing this platform")]
+	public Vector2 agentBounds = new Vector2(0.5f,1f);
+
+	[Tooltip("Show the pathing gizmos")]
+	public bool showPathingData = true;
+
+	public enum ConnectionType{
+		WALK,
+		JUMP,
+		DROP
+	}
+	public class PlatformConnection
+	{
+		public ConnectionType connectionType;
+		public PathablePlatform connectedPlatform;
+
+		public PlatformConnection(ConnectionType type, PathablePlatform platform){
+			connectionType = type;
+			connectedPlatform = platform;
+		}
+	}
+	private List<PlatformConnection> connections = new List<PlatformConnection>();
+	private Collider2D platformCollider;
+	private Vector3 oobBounds;
+
+	private Vector2 leftEdge;
+	private Vector2 rightEdge;
+
+	void Awake(){
+		platformCollider = GetComponent<Collider2D> ();
+		if (platformCollider == null) {
+			Debug.LogError ("Platform was unable to obtain a reference to it's collider");
+		}
+
+		// Find platforms that can be reached from here
+		FindConnectedPlatforms ();
+	}
+
+	#if UNITY_EDITOR
+	private float lastUpdateTime;
+	void OnDrawGizmosSelected(){
+		if (!showPathingData) {
+			return;
+		}
+		if (Time.time - 0.1 > lastUpdateTime) {
+			lastUpdateTime = Time.time;
+			FindConnectedPlatforms ();
+		}
+
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere (leftEdge, 0.2f);
+		Gizmos.DrawWireSphere (rightEdge, 0.2f);
+
+		for (int i = 0; i < connections.Count; i++) {
+			Gizmos.color = Color.red;
+			Gizmos.DrawLine (transform.position, connections [i].connectedPlatform.gameObject.transform.position);
+		}
+
+
+
+		if (gridUnit <= 0) {
+			return;
+		}
+		Gizmos.color = new Color (0, 0, 1, 0.2f);//Color.blue;
+		// short and low -> long and low -> short and high -> long and high
+		float searchWidth = agentJumpDistance;
+		for (float y = agentJumpHeight; y > -agentDropHeight; y -= gridUnit) {
+			for (float x = 0; Mathf.Abs(x) <= searchWidth; x -= gridUnit) {
+				Vector2 origin = leftEdge + (new Vector2 (x + (gridUnit * -0.5f), y - gridUnit * 0.5f));
+				Gizmos.DrawCube (origin, new Vector3 (gridUnit, gridUnit, gridUnit));
+				//Debug.DrawRay (origin, new Vector2(0,-0.2f), Color.blue, 10);
+			}
+			searchWidth += agentFloatDistance;
+		}
+
+		// short and low -> long and low -> short and high -> long and high
+		searchWidth = agentJumpDistance;
+		for (float y = agentJumpHeight; y > -agentDropHeight; y -= gridUnit) {
+			for (float x = 0; Mathf.Abs(x) <= searchWidth; x += gridUnit) {
+				Vector2 origin = rightEdge + (new Vector2 (x + (gridUnit * -0.5f), y - gridUnit * 0.5f));
+				Gizmos.DrawCube (origin, new Vector3 (gridUnit, gridUnit, gridUnit));
+				//Debug.DrawRay (origin, new Vector2(0,-0.2f), Color.blue, 10);
+			}
+			searchWidth += agentFloatDistance;
+		}
+
+	}
+	#endif
+
+	void FindConnectedPlatforms(){
+		connections.Clear ();
+
+		// Get the edges of the platform
+		EdgeCollider2D edgeCollider = platformCollider as EdgeCollider2D;
+		Quaternion rot = transform.rotation;
+		transform.rotation = Quaternion.identity;
+		if (edgeCollider != null) {
+			// Get the first and last points as the edges
+			leftEdge = edgeCollider.points[0];
+			rightEdge = edgeCollider.points [edgeCollider.pointCount - 1];
+		} else {
+			leftEdge = new Vector2 (-platformCollider.bounds.size.x, platformCollider.bounds.max.y - platformCollider.gameObject.transform.position.y) * 0.5f;
+			rightEdge = new Vector2 (platformCollider.bounds.size.x, platformCollider.bounds.max.y - platformCollider.gameObject.transform.position.y) * 0.5f;
+		}
+		oobBounds = platformCollider.bounds.extents * 2f;
+		if (oobBounds.y < 0.1f) {
+			oobBounds.y = 0.1f;
+		}
+		transform.rotation = rot;
+		leftEdge = transform.TransformPoint (leftEdge);
+		rightEdge = transform.TransformPoint (rightEdge);
+
+
+		// Cast the platform up to find platforms above
+		// Player can't jump up through platforms, neither should the npc
+		Vector2 origin = platformCollider.bounds.center;
+		float dist = agentJumpHeight;
+		RaycastHit2D[] hits = Physics2D.BoxCastAll (transform.position, oobBounds, transform.rotation.eulerAngles.z, Vector2.up, dist);
+		if (hits.Length > 0) {
+			for (int i = 0; i < hits.Length; i++) {
+				PathablePlatform platform = hits [i].collider.gameObject.GetComponent<PathablePlatform> ();
+				if (platform != null) {
+					PlatformConnection connection = new PlatformConnection (ConnectionType.JUMP, platform);
+					connections.Add (connection);
+				}
+			}
+		}
+
+		// Find platforms off the left edge
+		GetEdgePlatforms(leftEdge, Vector2.left);
+		// Find platforms off the right edge
+		GetEdgePlatforms(rightEdge, Vector2.right);
+	}
+
+	public void GetEdgePlatforms(Vector2 edge, Vector2 dir){
+		if (gridUnit <= 0) {
+			return;
+		}
+		// short and low -> long and low -> short and high -> long and high
+		float searchWidth = agentJumpDistance;
+		for (float y = agentJumpHeight; y > -agentDropHeight; y -= gridUnit) {
+			for (float x = 0; Mathf.Abs(x) <= searchWidth; x += gridUnit * dir.x) {
+				Vector2 origin = edge + (new Vector2 (x + (gridUnit * 0.5f * dir.x), y - gridUnit * 0.5f));
+				RaycastHit2D[] hits = Physics2D.RaycastAll (origin, Vector2.down, agentDropHeight - y);
+				if (hits.Length > 0) {
+					for (int i = 0; i < hits.Length; i++) {
+						PathablePlatform platform = hits[i].collider.gameObject.GetComponent<PathablePlatform>();
+						if (platform != null) {
+							connections.Add (new PlatformConnection (ConnectionType.JUMP, platform));
+						}
+					}
+				}
+			}
+			searchWidth += agentFloatDistance;
+		}
+	}
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -17,6 +17,9 @@ public class Controller : MonoBehaviour {
     private RaycastOrigins raycastOrigins;
     public CollisionInfo collisions;
 
+    public float maxClimbAngle = 80f;
+    public float maxDescendAngle = 80f;
+
 	void Start () {
         collider = GetComponent<BoxCollider2D>();
         CalculateRaySpacing();
@@ -25,6 +28,12 @@ public class Controller : MonoBehaviour {
     public void Move(Vector3 velocity) {
         UpdateRaycastOrigins();
         collisions.Reset();
+
+        collisions.velocityOld = velocity;
+
+        if (velocity.y < 0) {
+            DescendSlope(ref velocity);
+        }
         if (velocity.x != 0) {
             HorizontalCollisions(ref velocity);
         }
@@ -50,12 +59,39 @@ public class Controller : MonoBehaviour {
 
             // hits whichever collider is closest and stops there (prevents clipping)
             if (hit) {
-                velocity.x = (hit.distance - skinWidth) * directionX;
-                rayLength = hit.distance;
 
-                // sets the appropriate boolean based on evaluation of directionX == (1 or -1)
-                collisions.left = directionX == -1;
-                collisions.right = directionX == 1;
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+                if (i == 0 && slopeAngle <= maxClimbAngle) {
+                    // if we encounter an upward angle while descending another angle \*/ <-- we're between two slopes
+                    if(collisions.descendingSlope) {
+                        collisions.descendingSlope = false;
+                        velocity = collisions.velocityOld;
+                    }
+
+                    // makes actor flush against slope instead of floating
+                    float distanceToSlopeStart = 0;
+                    if (slopeAngle != collisions.slopeAngleOld) {
+                        distanceToSlopeStart = hit.distance - skinWidth;
+                        velocity.x -= distanceToSlopeStart * directionX;
+                    }
+                    ClimbSlope(ref velocity, slopeAngle);
+                    velocity.x += distanceToSlopeStart * directionX;
+                }
+
+                // if not climbing a slope or can't climb slope
+                if (!collisions.climbingSlope || slopeAngle > maxClimbAngle) {
+                    velocity.x = (hit.distance - skinWidth) * directionX;
+                    rayLength = hit.distance;
+
+                    // if already climbing and detect a collision (to the side)
+                    if (collisions.climbingSlope) {
+                        velocity.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x);
+                    }
+                    // sets the appropriate boolean based on evaluation of directionX == (1 or -1)
+                    collisions.left = directionX == -1;
+                    collisions.right = directionX == 1;
+                }
             }
         }
     }
@@ -78,9 +114,67 @@ public class Controller : MonoBehaviour {
                 velocity.y = (hit.distance - skinWidth) * directionY;
                 rayLength = hit.distance;
 
+                // if already climbing and detect a collision (above us)
+                if (collisions.climbingSlope) {
+                    velocity.x = velocity.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(velocity.x);
+                }
+
                 // sets the appropriate boolean based on evaluation of directionY == (1 or -1)
                 collisions.below = directionY == -1;
                 collisions.above = directionY == 1;
+            }
+        }
+        
+        if (collisions.climbingSlope) {
+            float directionX = Mathf.Sign(velocity.x);
+            rayLength = Mathf.Abs(velocity.x) + skinWidth;
+            Vector2 rayOrigin = ((directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight) + Vector3.up * velocity.y;
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+            if (hit) {
+                float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+                if(slopeAngle != collisions.slopeAngle) {
+                    velocity.x = (hit.distance - skinWidth) * directionX;
+                    collisions.slopeAngle = slopeAngle;
+                }
+            }
+        }
+    }
+
+    void ClimbSlope(ref Vector3 velocity, float slopeAngle) {
+        // how far in x we want to travel
+        float moveDistance = Mathf.Abs(velocity.x);
+        float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+        // if the player is jumping on slope
+        if (velocity.y <= climbVelocityY) {
+            velocity.y = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+            velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+            collisions.below = true;
+            collisions.climbingSlope = true;
+            collisions.slopeAngle = slopeAngle;
+        }
+    }
+
+    void DescendSlope(ref Vector3 velocity) {
+        float directionX = Mathf.Sign(velocity.x);
+        Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomRight : raycastOrigins.bottomLeft;
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, -Vector2.up, Mathf.Infinity, collisionMask);
+
+        if (hit) {
+            float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+            if (slopeAngle != 0 && slopeAngle <= maxDescendAngle) {
+                if (Mathf.Sign(hit.normal.x) == directionX) {
+                    if (hit.distance - skinWidth <= Mathf.Tan(slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x)) {
+                        float moveDistance = Mathf.Abs(velocity.x);
+                        float descendVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+                        velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+                        velocity.y -= descendVelocityY;
+
+                        collisions.slopeAngle = slopeAngle;
+                        collisions.descendingSlope = true;
+                        collisions.below = true;
+                    }
+                }
             }
         }
     }
@@ -117,9 +211,19 @@ public class Controller : MonoBehaviour {
         public bool above, below;
         public bool left, right;
 
+        public bool climbingSlope;
+        public bool descendingSlope;
+        public float slopeAngle, slopeAngleOld;
+        public Vector3 velocityOld;
+
         public void Reset() {
             above = below = false;
             left = right = false;
+            climbingSlope = false;
+            descendingSlope = false;
+
+            slopeAngleOld = slopeAngle;
+            slopeAngle = 0;
         }
     }
 }
