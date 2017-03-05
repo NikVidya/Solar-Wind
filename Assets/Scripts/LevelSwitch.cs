b10f7905eea3558594b33adb88ca2dd81a8f80e8
Soulist <soulistpvp@gmail.com>
2017-03-05 00:53:49 -0800

    Merging Intro and Assets
    Merging Intro and Assets

---
+++ Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSwitch : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+ 
+	public void LoadScene (string sceneName) {
+		Application.LoadLevel (sceneName);
+	}
+}
---
+++ Assets/Scripts/RoomSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSwitch : MonoBehaviour {
+
+	public string roomName;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D other){
+		Debug.Log("hit");
+		if (other.gameObject.name == "player"){
+			Application.LoadLevel (roomName);
+		}
+		if (other.gameObject.tag == "player"){
+			Application.LoadLevel (roomName);
+		}
+	}
+}
