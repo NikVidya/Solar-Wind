b65ae99ff00f4c85e3e8c0d51bb79a3ec353c50b
Labrasones <labrasones@gmail.com>
2017-03-18 11:02:55 -0700

    Added ambiance and footsteps sounds

---
+++ Assets/Scripts/AnimSoundPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AnimSoundPlayer : MonoBehaviour {
+
+	[System.Serializable]
+	public struct ClipGroup
+	{
+		[TooltipAttribute("Optional")]
+		public string groupName;
+
+		public AudioClip[] clips;
+	}
+	public ClipGroup[] clipGroups;
+
+	public void PlaySound(int groupIndex){
+		AudioClip clip = clipGroups[groupIndex].clips[ Random.Range(0, clipGroups[groupIndex].clips.Length - 1) ];
+		GetComponent<AudioSource> ().PlayOneShot (clip);
+	}
+}
